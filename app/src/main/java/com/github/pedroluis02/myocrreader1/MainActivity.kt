606907602612fcd63a5b267da3827e512767277d
Pedro Luis <pedroluisnr22@gmail.com>
2023-10-05 21:52:02 -0500

    feat: using camera view

--- app/src/main/java/com/github/pedroluis02/myocrreader1/MainActivity.kt
+++ app/src/main/java/com/github/pedroluis02/myocrreader1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.github.pedroluis02.myocrreader1
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,30 +10,86 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.github.pedroluis02.myocrreader1.camera.CameraPermissions
+import com.github.pedroluis02.myocrreader1.camera.CameraView
+import com.github.pedroluis02.myocrreader1.recognition.TextRecognizerController
 import com.github.pedroluis02.myocrreader1.ui.theme.MyOCRReaderTheme
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : ComponentActivity() {
+    private val tag = "MainActivity"
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var cameraPermissions: CameraPermissions
+
+    private lateinit var textRecognizer: TextRecognizerController
+
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyOCRReaderTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    if (shouldShowCamera.value) {
+                        CameraView(
+                            outputDirectory = outputDirectory,
+                            executor = cameraExecutor,
+                            onImageCaptured = ::handleImageCapture,
+                            onError = { Log.e(tag, "View error:", it) }
+                        )
+                    }
                 }
             }
         }
+
+        cameraPermissions = CameraPermissions(this)
+        cameraPermissions.request {
+            shouldShowCamera.value = true
+        }
+
+        outputDirectory = filesDir
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        textRecognizer = TextRecognizerController(this)
+    }
+
+    private fun handleImageCapture(uri: Uri) {
+        Log.i(tag, "Image captured: $uri")
+        //shouldShowCamera.value = false
+        textRecognizer.recognize(uri, {
+            Log.i(tag, "Text recognition:")
+            Log.i(tag, it)
+        }, failure = {
+            Log.e(tag, "Text recognition error:", it)
+        })
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun TextResult(text: String) {
+    Text(text = text)
 }
 
 @Preview(showBackground = true)
